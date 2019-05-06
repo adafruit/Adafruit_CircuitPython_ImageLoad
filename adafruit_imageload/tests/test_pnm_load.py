157ded5299f2e742463b9ed5340b7ecbd16f63ca
Matt Land <mland@sparefoot.com>
2019-05-06 16:10:42 -0400

    test cleanup

--- adafruit_imageload/tests/test_pnm_load.py
+++ adafruit_imageload/tests/test_pnm_load.py
@@ -12,43 +12,38 @@ class Bitmap(object):
 
 logging.getLogger().setLevel(logging.INFO)
 
-class TestPbmP1(TestCase):
+class TestPnmLoad(TestCase):
 
     def test_load_fails_with_no_header_data(self):
         f = BytesIO(b"some initial binary data: \x00\x01")
         try:
-            pnm.load(f, b'P1')
+            pnm.load(f, b'P1', bitmap=Bitmap)
             self.fail('should have failed')
         except Exception as e:
             if "Unsupported image format" not in str(e):
                 raise
 
-    def _test_load_works_p1_ascii(self):
+    def test_load_works_p1_ascii(self):
         test_file = os.path.join(os.path.dirname(__file__), '..', '..', 'examples', 'images', 'netpbm_p1_mono.pbm')
-        with open(test_file) as f:
-            f.seek(2)
-            bitmap, palette = pnm.load(f, b'P1')
-        self.assertTrue(isinstance(bitmap, Bitmap))
-        self.fail(bitmap)
+        with open(test_file, 'rb') as f:
+            bitmap, palette = pnm.load(f, b'P1', bitmap=Bitmap)
+        self.assertTrue(isinstance(bitmap, Bitmap), bitmap)
         self.assertEqual(1, bitmap.colors)
-        self.assertEqual(20, bitmap.width)
+        self.assertEqual(13, bitmap.width)
+        self.assertEqual(21, bitmap.height)
 
     def test_load_works_p4_binary(self):
         test_file = os.path.join(os.path.dirname(__file__), '..', '..', 'examples', 'images', 'netpbm_p4_mono.pbm')
-
         with open(test_file, 'rb') as f:
-            f.seek(2)
             bitmap, palette = pnm.load(f, b'P4', bitmap=Bitmap)
         self.assertTrue(isinstance(bitmap, Bitmap))
         self.assertEqual(1, bitmap.colors)
         self.assertEqual(8, bitmap.width)
         self.assertEqual(8, bitmap.height)
 
-    def test_load_works_p4_binary_big(self):
+    def test_load_works_p4_binary_high_res(self):
         test_file = os.path.join(os.path.dirname(__file__), '..', '..', 'examples', 'images', 'MARBLES.PBM')
-
         with open(test_file, 'rb') as f:
-            f.seek(2)
             bitmap, palette = pnm.load(f, b'P4', bitmap=Bitmap)
         self.assertTrue(isinstance(bitmap, Bitmap))
         self.assertEqual(1, bitmap.colors)
