5cb5683939dffcf520a8cfa25a4d1fac6967d13e
FoamyGuy <foamyguy@gmail.com>
2020-06-21 10:09:26 -0500

    make simpletest show the image

--- examples/imageload_simpletest.py
+++ examples/imageload_simpletest.py
@@ -1,6 +1,15 @@
+import board
 import displayio
 import adafruit_imageload
 
 image, palette = adafruit_imageload.load(
     "images/4bit.bmp", bitmap=displayio.Bitmap, palette=displayio.Palette
 )
+tile_grid = displayio.TileGrid(image, pixel_shader=palette)
+
+group = displayio.Group()
+group.append(tile_grid)
+board.DISPLAY.show(group)
+
+while True:
+    pass
