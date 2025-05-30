b06450917bee3a345d820ae7ae2d89335545d734
foamyguy <foamyguy@gmail.com>
2025-05-30 14:21:13 -0500

    displayio API update

--- examples/imageload_netpbm.py
+++ examples/imageload_netpbm.py
@@ -15,6 +15,7 @@ Tested with Feather M4 Express and 2.4" Featherwing
 import adafruit_ili9341
 import board
 import displayio
+import fourwire
 
 import adafruit_imageload
 
@@ -23,7 +24,7 @@ tft_cs = board.D9
 tft_dc = board.D10
 
 displayio.release_displays()
-display_bus = displayio.FourWire(spi, command=tft_dc, chip_select=tft_cs)
+display_bus = fourwire.FourWire(spi, command=tft_dc, chip_select=tft_cs)
 
 display = adafruit_ili9341.ILI9341(display_bus, width=320, height=240)
 
